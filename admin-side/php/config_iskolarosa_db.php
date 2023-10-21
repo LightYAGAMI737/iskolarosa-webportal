be3eed1da53ca8d34a1549614c8aca688f3a5f8a
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-10-22 03:51:59 +0800

    add config iskolarosa

--- admin-side/php/config_iskolarosa_db.php
+++ admin-side/php/config_iskolarosa_db.php
@@ -1,8 +1,8 @@
 <?php
 $server= "localhost";
-$username = "root";
-$password = "";
-$dbname = "iskolarosa_db";
+$username = "u789664695_iskolarosa_db";
+$password = "iSkolarosa-03";
+$dbname = "u789664695_iskolarosa_db";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
