e470374b33b0d4a48235a46f71ba66fcdfd3fa79
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-10-23 06:15:46 +0800

    fixed delete post confirm popup

--- admin-side/php/deletepostPopup.php
+++ admin-side/php/deletepostPopup.php
@@ -26,12 +26,12 @@
  <!-- confirmation of updated status -->
  <div class="popupOut" id="ConfrimMsgDeletePopUp">
     <div class="popupIn">
-      <i class="ri-checkbox-circle-fill" style="font-size: 6em; color: #0BA350;"></i>
+      <i class="ri-checkbox-circle-line" style="font-size: 6em; color: #0BA350;"></i>
       <strong>
-        <h2>Status Updated Successfully</h2>
+        <h2>Deleted Successfully</h2>
       </strong>
       <center>
-        <p>Applicant's status was updated successfully.</p>
+        <p>All selected post(s) have been deleted.</p>
       </center>
       <div style="padding: 3px;">
         <button type="button" id="okConfirm" >
