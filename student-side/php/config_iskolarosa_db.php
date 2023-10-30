4e84958870d9d0e82ca800e140a991a623740fb1
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-10-31 02:56:59 +0800

    updatestudent config

--- student-side/php/config_iskolarosa_db.php
+++ student-side/php/config_iskolarosa_db.php
@@ -1,9 +1,9 @@
 <?php
 
 $server= "localhost";
-$username = "root";
-$password = "";
-$dbname = "iskolarosa_db";
+$username = "u789664695_iskolarosa_db";
+$password = "iSkolarosa-03";
+$dbname = "u789664695_iskolarosa_db";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
