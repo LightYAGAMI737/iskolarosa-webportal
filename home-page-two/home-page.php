470ed9d6f5d81aca54ce592e3a5a766ac1b35f0e
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-11-01 03:51:45 +0800

    add homepage new design

---
+++ home-page-two/home-page.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Secular+One">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+    <link rel="stylesheet" href="home-page.css">
+    <link rel='stylesheet' href='../admin-side/css/remixicon.css'>
+
+    
+    <title>Homepage</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+    <a class="navbar-brand" href="#">
+            <img src="iskolarosa-logo.png" alt="My Logo" class="img-logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item" style="background-color: #A5040A; border-radius: 15px; ">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quick Guide</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Log in</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+      <div class="container mt-4">
+          <div class="row">
+              <div class="col-lg-5 col-md-12">
+                  <div class="flex-container">
+                  <div class="top-left-row">
+                    <h1 class="custom-text" style="color: #FEC021" >WELCOME</h1>
+                    <h1 class="custom-text" style="color: #A5040A">iSKOLARS!</h1>
+                </div>
+                <div class="bottom-left-row">
+                    <div class="left-card">
+                        <p>Application Qualification & Requirements</p>
+                        <button class="btn">
+                          <i class="ri-graduation-cap-fill" style="color: #A5040A"></i> 
+                          <span>College Educational Assistance Program</span>
+                        </button>
+                        <button class="btn mt-2">
+                          <i class="ri-graduation-cap-fill" style="color: #FEC021" ></i> 
+                          Libreng Pagpapaaral sa Pribadong Paaralan
+                        </button>
+                    </div>
+                </div>
+
+                  </div>
+              </div>
+              <div class="col-lg-7 col-md-12">
+                <div class="right-column">
+                    <div class="announcement-header">
+                        <p>Announcement</p>
+                    </div>
+                    <div class="announcement-outer-card" >
+
+                        <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>
+                        <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>
+                        <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>
+                        <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>     <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>     
+                        <div class="announcement-inner-card">
+                            <!-- Your announcement cards go here -->
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                            <p>Announcement</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+      </body>
+      </html>
