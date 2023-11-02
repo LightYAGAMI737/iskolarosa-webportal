5f897e554ebced20f7c7b25cadca9ae42766ff06
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-11-03 06:35:47 +0800

    homepage 2 base href

--- home-page-two/home-page.php
+++ home-page-two/home-page.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="https://iskolarosa.com/iskolarosa/home-page-two/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Secular+One">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
