fb2caf4d69b8b6228646c9d323ac0345a43d8b7a
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-11-16 20:34:13 +0800

    fix emailing in ceap-reg-form

--- ceap-reg-form/php/email_functions.php
+++ ceap-reg-form/php/email_functions.php
@@ -6,17 +6,7 @@
    // Function to send email using PHPMailer
    function sendEmail($email, $username, $password)
    {
-    $mail = new PHPMailer\PHPMailer\PHPMailer(); // Create a new PHPMailer instance
-   
-    // SMTP settings (replace these with your SMTP server details)
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; // Replace this with your SMTP server address
-    $mail->SMTPAuth = true;
-    $mail->Username = 'lancelirabackupfiles@gmail.com'; // Replace this with your SMTP username
-    $mail->Password = 'ccrvxxdtujkbqanw'; // Replace this with your SMTP password
-    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS; // Use SMTPS for port 465
-    $mail->Port = 465; // Use port 465 for SMTPS
-    
+   require_once '../../admin-side/php/PHPMailerConfigure.php';
        // Email content format
        $mail->IsHTML(true); // Set email format to HTML
    
