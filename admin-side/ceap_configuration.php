16b278c884bfc9e7d894f385a8c59a3cf0a79a7c
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-11-26 03:58:36 +0800

    added auto on/off set start date/time and end date/time for ceap config

--- admin-side/ceap_configuration.php
+++ admin-side/ceap_configuration.php
@@ -308,7 +308,37 @@ function checkToggleStateCEAP() {
 }
 // Call the function to check the toggle state
 checkToggleStateCEAP();
+
       </script>
+<!-- Include this script in your HTML file -->
+<script>
+// Function to periodically update the database state
+function updateDatabaseState() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './php/updateConfigStart.php', true);
+
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // Request was successful, you can log or handle the response
+           // console.log(xhr.responseText);
+        } else {
+            // Request failed, handle the error
+            //console.error('Error updating database state. Status:', xhr.status, 'Response:', xhr.responseText);
+        }
+    };
+
+    xhr.onerror = function () {
+        // Handle network errors
+        //console.error('Network error while updating database state.');
+    };
+
+    xhr.send();
+}
+
+// Set up a setInterval to periodically call the update function (every 5 minutes in this example)
+setInterval(updateDatabaseState, 1000); // Adjust the interval as needed
+
+</script>
 
       <script  src="./js/status_popup.js"></script>
       <script  src="./js/configurationPopup.js"></script>
--- admin-side/php/ceapconfigurationinsert.php
+++ admin-side/php/ceapconfigurationinsert.php
@@ -9,6 +9,9 @@ function sanitizeInput($input) {
     return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 }
 
+// Set the timezone to 'Asia/Manila'
+date_default_timezone_set('Asia/Manila');
+
 // Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve form data and sanitize for XSS prevention
@@ -19,8 +22,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $endDate = sanitizeInput($_POST['endDate']);
     $endTime = sanitizeInput($_POST['endTime']);
 
-    // Retrieve the toggle button value
-    $toggleValue = isset($_POST['toggleButton']) ? 1 : 0;
+    // Get the current date and time
+    $currentDateTime = date("Y-m-d H:i:s");
+
+    // Check if the current date and time are before the specified start time
+    if ($currentDateTime < "$startDate $startTime") {
+        $toggleValue = 0; // Uncheck the toggle button
+    } else {
+        $toggleValue = isset($_POST['toggleButton']) ? 1 : 0;
+    }
 
     // Perform SQL insertion with prepared statements
     $sql = "INSERT INTO ceap_configuration (qualifications, requirements, start_date, start_time, end_date, end_time, toggle_value) VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -51,4 +61,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 // Close the database connection
 mysqli_close($conn);
+
 ?>
