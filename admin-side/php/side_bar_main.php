31e21a001eb537e24c0bfce54d90e113bcbb82c8
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-05 16:44:56 +0800

    session timeout

--- admin-side/php/side_bar_main.php
+++ admin-side/php/side_bar_main.php
@@ -247,5 +247,5 @@ include 'logoutpopup.php';
         document.addEventListener('mousemove', AJXupdateLastActivityTime);
         document.addEventListener('keydown', AJXupdateLastActivityTime);
               // Check session timeout on an interval
-              setInterval(checkSessionTimeout, 5);
+              setInterval(AJXupdateLastActivityTime, 5);
     </script>
