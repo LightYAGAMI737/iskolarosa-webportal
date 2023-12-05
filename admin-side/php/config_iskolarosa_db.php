7c3e967be04881361877578326ba3229e6183245
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-05 14:55:55 +0800

    add timezone to asia/manila

--- admin-side/php/config_iskolarosa_db.php
+++ admin-side/php/config_iskolarosa_db.php
@@ -1,13 +1,17 @@
 <?php
-$server= "localhost";
+$server = "localhost";
 $username = "u789664695_iskolarosa_db";
 $password = "iSkolarosa-03";
 $dbname = "u789664695_iskolarosa_db";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
-if(!$conn) {
-    die("Connection Failed:".mysqli_connect_error());
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
 }
+
+// Set the session time zone to 'Asia/Manila'
+$query = "SET time_zone = '+08:00'";
+mysqli_query($conn, $query);
 
 ?>
