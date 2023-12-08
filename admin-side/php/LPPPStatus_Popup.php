e4fb0faeff0657cac5fa5c785705f354e71208b8
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-08 16:01:27 +0800

    update for confirm message

--- admin-side/php/LPPPStatus_Popup.php
+++ admin-side/php/LPPPStatus_Popup.php
@@ -1,5 +1,3 @@
-
-
 <!-- LPPPVerified popup -->
 <div class="popupOut" id="LPPPverifiedPopUp">
     <div class="popupIn">
@@ -140,6 +138,24 @@
 </div>
 
  <!-- confirmation of updated status -->
+ <div class="popupOut" id="ConfrimMsgPopUp">
+    <div class="popupIn">
+      <i class="ri-checkbox-circle-fill" style="font-size: 6em; color: #0BA350;"></i>
+      <strong>
+        <h2>Status Updated Successfully</h2>
+      </strong>
+      <center>
+        <p>Applicant's status was updated successfully.</p>
+      </center>
+      <div style="padding: 3px;">
+        <button type="button" id="okConfirm" >
+          <span>OK</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  
+ <!-- confirmation EXAM of updated status -->
  <div class="popupOut" id="ConfrimMsgEXAMPopUp">
     <div class="popupIn">
       <i class="ri-checkbox-circle-fill" style="font-size: 6em; color: #0BA350;"></i>
