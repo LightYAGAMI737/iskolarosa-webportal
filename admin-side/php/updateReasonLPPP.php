a5cfd6ed80f06bd401fb16d6354c56e97825f778
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-09 01:18:38 +0800

    update

--- admin-side/php/updateReasonLPPP.php
+++ admin-side/php/updateReasonLPPP.php
@@ -3,7 +3,6 @@
 session_start();
 
 include 'config_iskolarosa_db.php';
-require 'email_update_status_reason.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize user input
@@ -48,27 +47,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmtLog = $conn->prepare($logQuery);
         $stmtLog->bind_param("ssii", $previousStatus, $status, $applicantId, $employeeLogsId);
 
-        // Fetch the applicant's email address and control number from the database
-        $applicantEmailQuery = "SELECT active_email_address, control_number FROM lppp_reg_form WHERE lppp_reg_form_id = ?";
-        $stmtApplicantEmail = $conn->prepare($applicantEmailQuery);
-        $stmtApplicantEmail->bind_param("i", $applicantId);
-        $stmtApplicantEmail->execute();
-        $stmtApplicantEmail->bind_result($applicantEmail, $control_number);
-        $stmtApplicantEmail->fetch();
-        $stmtApplicantEmail->close();
-  
-        // Send an email to the applicant
-        $recipientEmail = $applicantEmail; // Use the fetched applicant email
-        $emailSent = sendEmail($recipientEmail, $control_number, $status, $reason);
-
-        if ($emailSent) {
             echo 'success'; // Update, log, and email sending were successful
-        } else {
-            echo 'email_error'; // Email sending failed, but the update and log were successful
-        }
-       } else {
-            echo 'error'; // Update failed
-        }
+        }echo 'error';
 }
 mysqli_close($conn);
 ?>
