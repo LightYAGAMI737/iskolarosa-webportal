242336072a1b94a8fe0d8f07f104bbaf4bf43b26
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-15 19:39:30 +0800

    add fail popup

---
+++ admin-side/js/updateStatusFail.js
@@ -0,0 +1,107 @@
+const reasonInputFail = document.getElementById("FailReason");
+const submitButtonFail = document.getElementById("submitReasonFail");
+reasonInputFail.addEventListener("input", function () {
+   if (reasonInputFail.value.length >= 5) {
+      submitButtonFail.classList.remove("disabled");
+      submitButtonFail.removeAttribute("disabled");
+   } else {
+      submitButtonFail.classList.add("disabled");
+      submitButtonFail.setAttribute("disabled", "disabled");
+   }
+});
+const FailPopUp = document.getElementById("FailPopUp");
+
+function openFailPopUp(status, reasonFail, applicantId) {
+   document.getElementById("reasonModalFail").style.display = "none";
+   FailPopUp.style.display = "block";
+}
+
+function closeFailPopUp() {
+   FailPopUp.style.display = "none";
+   openReasonModalFail(); // You should pass any required parameters to openReasonModalFail if needed
+}
+const cancelCloseButtonsFail = document.querySelectorAll("#Fail-cancel-button"); // Use querySelectorAll to select all matching elements
+cancelCloseButtonsFail.forEach((cancelButtonFail) => {
+   cancelButtonFail.addEventListener("click", closeFailPopUp);
+});
+const closeSymbolFail = document.querySelector(".close");
+if (closeSymbolFail) {
+   closeSymbolFail.addEventListener("click", function () {
+      const FailReasonInputFail = document.getElementById("FailReason");
+      if (FailReasonInputFail) {
+         FailReasonInputFail.value = ''; // Reset the input field
+      }
+      closereasonModalFail();
+   });
+}
+
+function closereasonModalFail() {
+   const reasonModalFail = document.getElementById("reasonModalFail");
+   reasonModalFail.style.display = "none";
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const confirmButtonFail = document.getElementById("confirmButtonFail");
+    if (confirmButtonFail) {
+        confirmButtonFail.addEventListener("click", function () {
+            const status = "Fail"; // You can adjust this value as needed
+            const reasonFail = document.getElementById("FailReason").value;
+            const applicantId = ceapRegFormId; // Access the value from PHP
+            submitStatusAndReason(status, reasonFail, applicantId);
+        });
+    }
+});
+
+//Fail and fail
+let reasonFail;
+function openReasonModalFail(status) {
+   const modal = document.getElementById("reasonModalFail");
+   if (modal) {
+      modal.style.display = "block";
+      // Pass the status to the openFailPopUp function only if it's defined and not empty
+      if (status) {
+         const submitReasonFailButton = document.getElementById("submitReasonFail");
+         if (submitReasonFailButton) {
+            submitReasonFailButton.onclick = function () {
+               reasonFail = document.getElementById("FailReason").value;
+               if (reasonFail.trim() !== '') {
+                  const applicantId = ceapRegFormId;
+                  openFailPopUp(status, reasonFail, applicantId);
+               } else {
+                  alert('Please enter a reasonFail.');
+               }
+            };
+         } else {
+            console.error("Element with ID 'submitReasonFail' not found.");
+         }
+      }
+   } else {
+      console.error("Element with ID 'reasonModalFail' not found.");
+   }
+}
+
+function submitStatusAndReason(status, reasonFail, applicantId) {
+   // Send an AJAX request to update both status and reasonFail
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", "../../../php/updateReasonFail.php", true);
+   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+         var response = xhr.responseText.trim();
+         console.log("Response from AJAX:", response); // Log the response
+         if (response === 'success') {
+            closeFailPopUp();
+            closereasonModalFail();
+            openConfirmationPopup();
+         } else {
+            alert('Failed to update status and reasonFail.');
+         }
+      } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
+         console.error("AJAX request failed with status:", xhr.status); // Log AJAX errors
+      }
+   };
+   // Send the AJAX request with status, reasonFail, and applicantId
+   xhr.send("status=" + status + "&id=" + applicantId + "&reasonFail=" + reasonFail);
+}
+
---
+++ admin-side/php/updateReasonFail.php
@@ -0,0 +1,76 @@
+<?php
+// Start the session
+session_start();
+
+include 'config_iskolarosa_db.php';
+require_once 'email_update_status_reason.php'; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize user input
+// Sanitize user input
+$status = htmlspecialchars($_POST["status"], ENT_QUOTES, 'UTF-8');
+$applicantId = filter_input(INPUT_POST, "id", FILTER_VALIDATE_INT);
+$reasonFail = htmlspecialchars($_POST["reasonFail"], ENT_QUOTES, 'UTF-8');
+$employeeUsername = $_SESSION["username"];
+
+    
+    // Check if input validation failed
+    if ($status === false || $applicantId === false || $reasonFail === false || $applicantId === null) {
+        $response = 'error'; // Invalid input
+    }
+
+        // Retrieve the previous status from the database
+        $prevStatusQuery = "SELECT status FROM temporary_account WHERE ceap_reg_form_id = ?";
+        $stmtPrev = $conn->prepare($prevStatusQuery);
+        $stmtPrev->bind_param("i", $applicantId);
+        $stmtPrev->execute();
+        $stmtPrev->bind_result($previousStatus);
+        $stmtPrev->fetch();
+        $stmtPrev->close();
+
+        // Update the status and reason in the database using prepared statements
+        $updateQuery = "UPDATE temporary_account SET status = ?, reason = ? WHERE ceap_reg_form_id = ?";
+        $stmt = $conn->prepare($updateQuery);
+        $stmt->bind_param("ssi", $status, $reasonFail, $applicantId);
+
+        if ($stmt->execute()) {
+            // Retrieve the employee_logs_id based on the employee username
+            $employeeIdQuery = "SELECT employee_logs_id FROM employee_logs WHERE employee_username = ?";
+            $stmtEmployeeId = $conn->prepare($employeeIdQuery);
+            $stmtEmployeeId->bind_param("s", $employeeUsername);
+            $stmtEmployeeId->execute();
+            $stmtEmployeeId->bind_result($employeeLogsId);
+            $stmtEmployeeId->fetch();
+            $stmtEmployeeId->close();
+
+            // Log the status change in the applicant_status_logs table
+            $logQuery = "INSERT INTO applicant_status_logs (previous_status, updated_status, ceap_reg_form_id, employee_logs_id) VALUES (?, ?, ?, ?)";
+            $stmtLog = $conn->prepare($logQuery);
+            $stmtLog->bind_param("ssii", $previousStatus, $status, $applicantId, $employeeLogsId);
+            $stmtLog->execute();
+            
+          // Fetch the applicant's email address and control number from the database
+          $applicantEmailQuery = "SELECT active_email_address, control_number FROM ceap_reg_form WHERE ceap_reg_form_id = ?";
+          $stmtApplicantEmail = $conn->prepare($applicantEmailQuery);
+          $stmtApplicantEmail->bind_param("i", $applicantId);
+          $stmtApplicantEmail->execute();
+          $stmtApplicantEmail->bind_result($applicantEmail, $control_number);
+          $stmtApplicantEmail->fetch();
+          $stmtApplicantEmail->close();
+  
+          // Send an email to the applicant
+          $recipientEmail = $applicantEmail; // Use the fetched applicant email
+          $emailSent = sendEmail($recipientEmail, $control_number, $status, $reasonFail);
+  
+          if ($emailSent) {
+              echo 'success'; // Update, log, and email sending were successful
+          } else {
+              echo 'email_error'; // Email sending failed, but the update and log were successful
+          }
+         } else {
+              echo 'error'; // Update failed
+          }
+  }
+  
+  mysqli_close($conn);
+?>
