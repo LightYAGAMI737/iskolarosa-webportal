2ceb682b930981c9f699ff10a7fa4950b8c394a8
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2023-12-27 11:19:10 +0800

    add old applicants

---
+++ admin-side/APPLICANTS/BARANGAY/BALIBAGO/ceap_list_fail.php
@@ -0,0 +1,153 @@
+<?php
+   session_start();
+   // Check if the session is not set (user is not logged in)
+   if (!isset($_SESSION['username'])) {
+       echo 'You need to log in to access this page.';
+       exit();
+   }
+   include '../../../php/config_iskolarosa_db.php';
+   
+   $currentStatus = 'Fail';
+   $currentPage = 'ceap_list';
+   $currentSubPage = 'new applicant';
+   $currentDirectory = basename(__DIR__);
+   $currentBarangay = $currentDirectory;
+   
+   // Assuming $currentStatus is also a variable you need to sanitize
+$currentStatus = mysqli_real_escape_string($conn, $currentStatus);
+   // Modify the SQL query to include the selected barangay
+   $query = "SELECT t.*, UPPER(p.first_name) AS first_name, UPPER(p.last_name) AS last_name, UPPER(p.barangay) AS barangay, p.control_number, p.date_of_birth, UPPER(t.status) AS status
+   FROM ceap_reg_form p
+   INNER JOIN temporary_account t ON p.ceap_reg_form_id = t.ceap_reg_form_id
+   WHERE p.barangay = ? AND t.status = ?";
+   $stmt = mysqli_prepare($conn, $query);
+   mysqli_stmt_bind_param($stmt, "ss", $currentBarangay, $currentStatus);
+   mysqli_stmt_execute($stmt);
+   $result = mysqli_stmt_get_result($stmt);
+
+   ?>
+<!DOCTYPE html>
+<html lang="en" >
+   <head>
+      <meta charset="UTF-8">
+      <title>iSKOLAROSA | <?php echo strtoupper($currentSubPage); ?></title>
+      <link rel="icon" href="../../../system-images/iskolarosa-logo.png" type="image/png">
+      <link rel='stylesheet' href='../../../css/remixicon.css'>
+      <link rel='stylesheet' href="../../../css/unpkg-layout.css">
+      <link rel="stylesheet" href="../../../css/side_bar.css">
+      <link rel="stylesheet" href="../../../css/ceap_list.css">
+   </head>
+   <body>
+      <?php 
+         include '../../side_bar_barangay.php';
+         
+         ?>
+      <!-- home content-->    
+      <div class="form-group">
+         <input type="text" name="search" class="form-control" id="search" placeholder="Search by Control Number or Last name"  oninput="formatInput(this)">
+         <button type="button" class="btn btn-primary" onclick="searchApplicants()">Search</button>
+      </div>
+      <!-- table for displaying the applicant list -->
+      <div class="background">
+         <h2 style="text-align: center">CEAP APPLICANT LIST</h2>
+         <?php
+            if (mysqli_num_rows($result) === 0) {
+              // Display the empty state image and message
+              echo '<div class="empty-state">';
+              echo '<img src="../../../../empty-state-img/no_applicant.png" alt="No records found" class="empty-state-image">';
+              echo '<p>No applicant found.</p>';
+              echo '</div>';
+            } else {
+              ?>
+         <div class="applicant-info">
+            <table>
+               <tr>
+                  <th>NO.</th>
+                  <th>CONTROL NUMBER</th>
+                  <th>LAST NAME</th>
+                  <th>FIRST NAME</th>
+                  <!-- <th>BARANGAY</th>
+                  <th>STATUS</th> -->
+               </tr>
+               <?php
+                  $counter = 1;
+                  
+                           // Display applicant info using a table
+                           while ($row = mysqli_fetch_assoc($result)) {
+                              echo '<tr class="applicant-row contents" onclick="seeMore(\'' . $row['ceap_reg_form_id'] . '\')" style="cursor: pointer;">';
+                              echo '<td><strong>' . $counter++ . '</strong></td>';
+                              echo '<td>' . strtoupper($row['control_number']) . '</td>';
+                              echo '<td>' . strtoupper($row['last_name']) . '</td>';
+                              echo '<td>' . strtoupper($row['first_name']) . '</td>';
+                              // echo '<td>' . strtoupper($row['barangay']) . '</td>';
+                              // echo '<td>' . strtoupper($row['status']) . '</td>';
+                              echo '</tr>';
+                           }
+                           ?>
+            </table>
+            <div id="noApplicantFound" style="display: none; text-align: center; margin-top: 10px;">
+               No applicant found.
+            </div>
+         </div>
+      </div>
+      <?php } ?>
+      <!-- end applicant list -->
+      <footer class="footer">
+      </footer>
+      </main>
+      <div class="overlay"></div>
+      </div>
+      <!-- partial -->
+      <script src='../../../js/unpkg-layout.js'></script><script  src="../../../js/side_bar.js"></script>
+
+
+      <script>
+         function seeMore(id) {
+             // Redirect to a page where you can retrieve the reserved data based on the given ID
+             window.location.href = "ceap_information.php?ceap_reg_form_id=" + id;
+         }
+         
+      </script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script>
+         $(document).ready(function() {
+             // Add an event listener to the search input field
+             $('#search').on('input', function() {
+                 searchApplicants();
+             });
+         });
+         
+         function searchApplicants() {
+             var searchValue = $('#search').val().toUpperCase();
+             var found = false; // Flag to track if any matching applicant is found
+             $('.contents').each(function () {
+                 var controlNumber = $(this).find('td:nth-child(2)').text().toUpperCase();
+                 var lastName = $(this).find('td:nth-child(3)').text().toUpperCase();
+                 if (searchValue.trim() === '' || controlNumber.includes(searchValue) || lastName.includes(searchValue)) {
+                     $(this).show();
+                     found = true;
+                 } else {
+                     $(this).hide();
+                 }
+             });
+         
+             // Display "No applicant found" message if no matching applicant is found
+             if (!found) {
+                 $('#noApplicantFound').show();
+             } else {
+                 $('#noApplicantFound').hide();
+             }
+         }
+         
+         function formatInput(inputElement) {
+             // Remove multiple consecutive white spaces
+             inputElement.value = inputElement.value.replace(/\s+/g, ' ');
+         
+             // Convert input text to uppercase
+             inputElement.value = inputElement.value.toUpperCase();
+           }
+         
+         
+      </script>
+   </body>
+</html>
---
+++ admin-side/APPLICANTS/BARANGAY/LABAS/updateStatusInterview.php
@@ -0,0 +1,115 @@
+<?php
+ if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include '../../../php/config_iskolarosa_db.php';
+    require_once '../../../php/email_update_status_interview.php'; 
+   
+    $currentStatus = 'Verified';
+    $currentDirectory = basename(__DIR__);
+    $currentBarangay = $currentDirectory;
+    // Assuming $currentStatus is also a variable you need to sanitize
+    $currentStatus = mysqli_real_escape_string($conn, $currentStatus);
+    
+    // Process and update interview dates
+    $interviewDate = $_POST['interview_date'];
+    $interview_hours = $_POST['interview_hours'];
+    $interview_minutes = $_POST['interview_minutes'];
+    $interview_ampm = $_POST['interview_ampm'];
+    $limit = $_POST['limit'];
+    
+    if (!empty($interviewDate) && !empty($interview_hours) && !empty($interview_minutes) && !empty($interview_ampm) && !empty($limit)) {
+        $interviewDate = mysqli_real_escape_string($conn, $interviewDate);
+        $limit = intval($limit);
+    
+        $qualifiedQuery = "SELECT t.*, UPPER(p.first_name) AS first_name, UPPER(p.last_name) AS last_name, UPPER(p.barangay) AS barangay, p.control_number, p.date_of_birth, p.ceap_reg_form_id
+        FROM ceap_reg_form p
+        INNER JOIN temporary_account t ON p.ceap_reg_form_id = t.ceap_reg_form_id
+        WHERE t.status = ? AND p.barangay = ?
+        LIMIT ?";
+        
+        $stmt = mysqli_prepare($conn, $qualifiedQuery);
+        mysqli_stmt_bind_param($stmt, "ssi", $currentStatus, $currentBarangay, $limit);
+        mysqli_stmt_execute($stmt);
+        $qualifiedResult = mysqli_stmt_get_result($stmt);
+    
+        $updateCount = 0; // Track the number of applicants updated
+
+        while ($row = mysqli_fetch_assoc($qualifiedResult)) {
+            if ($updateCount >= $limit) {
+                break; // Stop updating once the limit is reached
+            }
+
+            $ceapRegFormId = $row['ceap_reg_form_id'];
+            $employeeUsername = $_SESSION["username"];
+
+            // Retrieve the previous status from the database
+            $prevStatusQuery = "SELECT status FROM temporary_account WHERE ceap_reg_form_id = ?";
+            $stmtPrev = $conn->prepare($prevStatusQuery);
+            $stmtPrev->bind_param("i", $ceapRegFormId);
+            $stmtPrev->execute();
+            $stmtPrev->bind_result($previousStatus);
+            $stmtPrev->fetch();
+            $stmtPrev->close();
+
+            $updateTimeQuery = "UPDATE temporary_account SET interview_date = ?, interview_hour = ?, interview_minute = ?, interview_period = ? WHERE ceap_reg_form_id = ?";
+            $stmtTimeUpdate = mysqli_prepare($conn, $updateTimeQuery);
+            mysqli_stmt_bind_param($stmtTimeUpdate, "siisi", $interviewDate, $interview_hours, $interview_minutes, $interview_ampm, $ceapRegFormId);
+            mysqli_stmt_execute($stmtTimeUpdate);
+
+            // Update the status to 'interview'
+            $statusUpdateQuery = "UPDATE temporary_account SET status = 'interview' WHERE ceap_reg_form_id = ?";
+            $stmtStatusUpdate = mysqli_prepare($conn, $statusUpdateQuery);
+            mysqli_stmt_bind_param($stmtStatusUpdate, "i", $ceapRegFormId);
+            mysqli_stmt_execute($stmtStatusUpdate);
+
+            mysqli_stmt_close($stmtTimeUpdate);
+            mysqli_stmt_close($stmtStatusUpdate);
+
+            if ($stmt->affected_rows > 0) {
+                // Retrieve the employee_logs_id based on the employee username
+                $employeeIdQuery = "SELECT employee_logs_id FROM employee_logs WHERE employee_username = ?";
+                $stmtEmployeeId = $conn->prepare($employeeIdQuery);
+                $stmtEmployeeId->bind_param("s", $employeeUsername);
+                $stmtEmployeeId->execute();
+                $stmtEmployeeId->bind_result($employeeLogsId);
+                $stmtEmployeeId->fetch();
+                $stmtEmployeeId->close();
+
+                $status = 'interview'; // Assign a value to $status before the insert query
+                // Log the status change in the applicant_status_logs table using a prepared statement
+                $logQuery = "INSERT INTO applicant_status_logs (previous_status, updated_status, ceap_reg_form_id, employee_logs_id) VALUES (?, ?, ?, ?)";
+                $stmtLog = $conn->prepare($logQuery);
+                $stmtLog->bind_param("ssii", $previousStatus, $status, $ceapRegFormId, $employeeLogsId);
+                $stmtLog->execute();
+
+                // Fetch the applicant's email address and control number from the database
+                $applicantEmailQuery = "SELECT active_email_address, control_number FROM ceap_reg_form WHERE ceap_reg_form_id = ?";
+                $stmtApplicantEmail = $conn->prepare($applicantEmailQuery);
+                $stmtApplicantEmail->bind_param("i", $ceapRegFormId); // Corrected variable name
+                $stmtApplicantEmail->execute();
+                $stmtApplicantEmail->bind_result($applicantEmail, $control_number);
+                $stmtApplicantEmail->fetch();
+                $stmtApplicantEmail->close();
+
+                // Send an email to the applicant
+                $recipientEmail = $applicantEmail; // Use the fetched applicant email
+                $emailSent = sendEmailInterview($recipientEmail, $control_number, $status, $interviewDate);
+
+                if ($emailSent) {
+                    echo 'success'; // Update, log, and email sending were successful
+                } else {
+                    echo 'email_error'; // Email sending failed, but the update and log were successful
+                }
+                } else {
+                    echo 'error'; // Update failed
+                }
+                $updateCount++;
+
+}
+
+    }
+}
+?>
