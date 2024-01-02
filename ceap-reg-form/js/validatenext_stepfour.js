b8b50a393c7e98febcc37cd1afd5f49823336523
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2024-01-02 20:31:09 +0800

    fix file name "fakepath"

--- ceap-reg-form/js/validatenext_stepfour.js
+++ ceap-reg-form/js/validatenext_stepfour.js
@@ -17,8 +17,6 @@
     var barangay = document.getElementById("barangay").value;
     var municipality = document.getElementById("municipality").value;
     var province = document.getElementById("province").value;
-    var uploadVotersApplicant = document.getElementById("uploadVotersApplicant").value;
-    var uploadPhotoJPG = document.getElementById("uploadPhotoJPG").value;
     //school
     var elementarySchool = document.getElementById("elementary_school").value;
     var elementaryYear = document.getElementById("elementary_year").value;
@@ -36,8 +34,6 @@
     var schoolAddress = document.getElementById("school_address").value;
     var schoolType = document.getElementById("school_type").value;
     var studentIdNo = document.getElementById("student_id_no").value;
-    var uploadGrade = document.getElementById("uploadGrade").value;
-    var uploadCOR = document.getElementById("uploadCOR").value;
     //family
     var guardianFirstName = document.getElementById("guardian_firstname").value;
     var guardianLastName = document.getElementById("guardian_lastname").value;
@@ -45,6 +41,19 @@
     var guardianOccupation = document.getElementById("guardian_occupation").value;
     var guardianMonthlyIncome = document.getElementById("guardian_monthly_income").value;
     var guardianAnnualIncome = document.getElementById("guardian_annual_income").value;
+
+    // Extract file name from the full path for the file inputs
+    var uploadVotersApplicant = document.getElementById("uploadVotersApplicant").value;
+    var uploadPhotoJPG = document.getElementById("uploadPhotoJPG").value;
+
+    uploadVotersApplicant = getFileName(uploadVotersApplicant);
+    uploadPhotoJPG = getFileName(uploadPhotoJPG);
+    var uploadGrade = getFileName(document.getElementById("uploadGrade").value);
+    var uploadCOR = getFileName(document.getElementById("uploadVotersParent").value);
+    var uploadVotersParent = getFileName(document.getElementById("uploadVotersParent").value);
+    var uploadITR = getFileName(document.getElementById("uploadITR").value);
+    var uploadResidency = getFileName(document.getElementById("uploadResidency").value);
+
 
     // Create a container element to display the review information
     var reviewContainer = document.getElementById("review-container");
@@ -66,8 +75,6 @@
         tableHTML+= '<tr><td>Barangay:</td><td>' + barangay + '</td></tr>';
         tableHTML+= '<tr><td>Municipality:</td><td>' + municipality + '</td></tr>';
         tableHTML+= '<tr><td>Province:</td><td>' + province  + '</td></tr>';
-        tableHTML+= '<tr><td>Applicant\'s Voter Certificate:</td><td>' + uploadVotersApplicant + '</td></tr>';
-        tableHTML+= '<tr><td>Upload 2x2 Picture:</td><td>' + uploadPhotoJPG + '</td></tr>';
         //school
         tableHTML+= '<tr><td>Elementary School:</td><td>' + elementarySchool + '</td></tr>';
         tableHTML+= '<tr><td>Year Graduated (Elementary):</td><td>' + elementaryYear + '</td></tr>';
@@ -85,8 +92,6 @@
         tableHTML+= '<tr><td>School Address:</td><td>' + schoolAddress + '</td></tr>';
         tableHTML+= '<tr><td>School Type:</td><td>' + schoolType + '</td></tr>';
         tableHTML+= '<tr><td>Student ID No.:</td><td>' + studentIdNo + '</td></tr>';
-        tableHTML+= '<tr><td>Upload Current Grade:</td><td>' + uploadGrade + '</td></tr>';
-        tableHTML+= '<tr><td>Upload Certificate of Registration:</td><td>' + uploadCOR + '</td></tr>';
         //family
         tableHTML+= '<tr><td>Guardian First Name:</td><td>' + guardianFirstName + '</td></tr>';
         tableHTML+= '<tr><td>Guardian Last Name:</td><td>' + guardianLastName + '</td></tr>';
@@ -94,6 +99,16 @@
         tableHTML+= '<tr><td>Occupation:</td><td>' + guardianOccupation + '</td></tr>';
         tableHTML+= '<tr><td>Monthly Income:</td><td>' + guardianMonthlyIncome + '</td></tr>';
         tableHTML+= '<tr><td>Annual Income:</td><td>' + guardianAnnualIncome + '</td></tr>';
+
+        // Update the tableHTML with the extracted file names
+    tableHTML += '<tr><td>Applicant\'s Voter Certificate:</td><td>' + uploadVotersApplicant + '</td></tr>';
+    tableHTML += '<tr><td>Upload 2x2 Picture:</td><td>' + uploadPhotoJPG + '</td></tr>';
+    tableHTML += '<tr><td>Upload Current Grade:</td><td>' + uploadGrade + '</td></tr>';
+    tableHTML += '<tr><td>Upload Certificate of Registration:</td><td>' + uploadCOR + '</td></tr>';
+    tableHTML += '<tr><td>Upload Guardian\'s Voter Certificate:</td><td>' + uploadVotersParent + '</td></tr>';
+    tableHTML += '<tr><td>Upload Guardian\'s Income Tax Return:</td><td>' + uploadITR + '</td></tr>';
+    tableHTML += '<tr><td>Upload Guardian\'s Voter Certificate:</td><td>' + uploadResidency + '</td></tr>';
+
     // Clear the review container before adding new content
     reviewContainer.innerHTML = "";
 
@@ -106,6 +121,14 @@
     reviewContainer.innerHTML = tableHTML;
 }
 
+displayUserInput();
+
+// Helper function to get the file name from the full path
+function getFileName(fullPath) {
+  // Using a regular expression to extract the file name
+  var match = fullPath.match(/[^\\/]*$/);
+  return match ? match[0] : fullPath;
+}
 // Add an event listener to each input field
 document.getElementById("last_name").addEventListener("input", displayUserInput);
 document.getElementById("first_name").addEventListener("input", displayUserInput);
@@ -123,8 +146,6 @@ document.getElementById("house_number").addEventListener("input", displayUserInp
 document.getElementById("barangay").addEventListener("input", displayUserInput);
 document.getElementById("municipality").addEventListener("input", displayUserInput);
 document.getElementById("province").addEventListener("input", displayUserInput);
-document.getElementById("uploadVotersApplicant").addEventListener("change", displayUserInput);
-document.getElementById("uploadPhotoJPG").addEventListener("change", displayUserInput);
 //school
 document.getElementById("elementary_school").addEventListener("input", displayUserInput);
 document.getElementById("elementary_year").addEventListener("change", displayUserInput);
@@ -142,8 +163,6 @@ document.getElementById("school_name").addEventListener("input", displayUserInpu
 document.getElementById("school_address").addEventListener("input", displayUserInput);
 document.getElementById("school_type").addEventListener("change", displayUserInput);
 document.getElementById("student_id_no").addEventListener("input", displayUserInput);
-document.getElementById("uploadGrade").addEventListener("change", displayUserInput);
-document.getElementById("uploadCOR").addEventListener("change", displayUserInput);
 //family
 // Add event listeners for the input fields in the "Head of the Family" section
 document.getElementById("guardian_firstname").addEventListener("input", displayUserInput);
@@ -152,8 +171,16 @@ document.getElementById("guardian_relationship").addEventListener("input", displ
 document.getElementById("guardian_occupation").addEventListener("input", displayUserInput);
 document.getElementById("guardian_monthly_income").addEventListener("input", displayUserInput);
 document.getElementById("guardian_annual_income").addEventListener("input", displayUserInput);
+// Add an event listener to each file input field
+document.getElementById("uploadVotersApplicant").addEventListener("change", displayUserInput);
+document.getElementById("uploadPhotoJPG").addEventListener("change", displayUserInput);
+document.getElementById("uploadGrade").addEventListener("change", displayUserInput);
+document.getElementById("uploadVotersParent").addEventListener("change", displayUserInput);
+document.getElementById("uploadITR").addEventListener("change", displayUserInput);
+document.getElementById("uploadCOR").addEventListener("change", displayUserInput);
+document.getElementById("uploadResidency").addEventListener("change", displayUserInput);
 
-displayUserInput();
+
 
 document.addEventListener("DOMContentLoaded", function () {
     var confirmButton = document.getElementById('submitReview');
