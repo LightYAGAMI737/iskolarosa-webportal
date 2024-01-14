667ed07bf254b942b47c314c5ef747587dae6553
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2024-01-14 14:42:05 +0800

    add popups for rescheduling

--- admin-side/php/reschedulepopups.php
+++ admin-side/php/reschedulepopups.php
@@ -16,12 +16,12 @@
     </div>
   </div>
 
-<!-- Reschedule Aplaya popup  -->
-<div class="popupOut" id="RescheduleAplayaPopUp">
+<!-- Reschedule Current popup  -->
+<div class="popupOut" id="RescheduleCurrentPopUp">
     <div class="popupIn">
         <i class="ri-question-fill" style="font-size: 6em; color: #F54021;"></i>
         <strong>
-            <h2>Reschedule Applicant?</h2>
+            <h2>Reschedule Current Applicant?</h2>
         </strong>
         <center>
             <p>Are you sure you want to proceed with the rescheduling of applicant(s)?</p>
@@ -31,7 +31,7 @@
                 <i class="ri-close-fill"></i>
                 <span>Cancel</span>
             </button>
-            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterview()">
+            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterviewCurrent()">
                 <i class="ri-check-fill"></i>
                 <span>Confirm</span>
             </button>
@@ -39,12 +39,12 @@
     </div>
 </div>
 
-<!-- Reschedule OLD popup  -->
-<div class="popupOut" id="RescheduleOLDPopUp">
+<!-- Reschedule OLDCurrent popup  -->
+<div class="popupOut" id="RescheduleOLDCurrentPopUp">
     <div class="popupIn">
         <i class="ri-question-fill" style="font-size: 6em; color: #F54021;"></i>
         <strong>
-            <h2>Reschedule Applicant? OLD</h2>
+            <h2>Reschedule Current Applicant?</h2>
         </strong>
         <center>
             <p>Are you sure you want to proceed with the rescheduling of applicant(s)?</p>
@@ -54,7 +54,53 @@
                 <i class="ri-close-fill"></i>
                 <span>Cancel</span>
             </button>
-            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterviewOLD()">
+            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterviewOLDCurrent()">
+                <i class="ri-check-fill"></i>
+                <span>Confirm</span>
+            </button>
+        </div>
+    </div>
+</div>
+
+<!-- Reschedule Past popup  -->
+<div class="popupOut" id="ReschedulePastPopUp">
+    <div class="popupIn">
+        <i class="ri-question-fill" style="font-size: 6em; color: #F54021;"></i>
+        <strong>
+            <h2>Reschedule Past Applicant?</h2>
+        </strong>
+        <center>
+            <p>Are you sure you want to proceed with the rescheduling of applicant(s)?</p>
+        </center>
+        <div style="padding: 3px;">
+            <button type="button" class="cancel-button" style="margin-right: 15px; background-color: #C0C0C0;">
+                <i class="ri-close-fill"></i>
+                <span>Cancel</span>
+            </button>
+            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterviewPast()">
+                <i class="ri-check-fill"></i>
+                <span>Confirm</span>
+            </button>
+        </div>
+    </div>
+</div>
+
+<!-- Reschedule OLDPast popup  -->
+<div class="popupOut" id="RescheduleOLDPastPopUp">
+    <div class="popupIn">
+        <i class="ri-question-fill" style="font-size: 6em; color: #F54021;"></i>
+        <strong>
+            <h2>Reschedule Past Applicant?</h2>
+        </strong>
+        <center>
+            <p>Are you sure you want to proceed with the rescheduling of applicant(s)?</p>
+        </center>
+        <div style="padding: 3px;">
+            <button type="button" class="cancel-button" style="margin-right: 15px; background-color: #C0C0C0;">
+                <i class="ri-close-fill"></i>
+                <span>Cancel</span>
+            </button>
+            <button type="button" class="confirm-button saveBtn" onclick="RescheduleInterviewOLDPast()">
                 <i class="ri-check-fill"></i>
                 <span>Confirm</span>
             </button>
