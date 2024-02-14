bbdfb4a14cf578a7ed166ded127a74e1a3536e23
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2024-02-14 19:45:08 +0800

    update

--- ceap-reg-form/js/validatenext_stepfour.js
+++ ceap-reg-form/js/validatenext_stepfour.js
@@ -184,7 +184,7 @@ document.getElementById("uploadResidency").addEventListener("change", displayUse
 
 document.addEventListener("DOMContentLoaded", function () {
     var confirmButton = document.getElementById('submitReview');
-    var cooldownSeconds = 1;
+    var cooldownSeconds = 2;
     var countdownInterval;
 
     // Function to update the button text and handle cooldown
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const previousBTN = document.getElementById('previous-stepthree')
     previousBTN.addEventListener('click', function () {
         clearInterval(countdownInterval); // Clear the countdown interval when going back
-        cooldownSeconds = 1; // Reset the cooldown timer
+        cooldownSeconds = 2; // Reset the cooldown timer
         confirmButton.classList.remove("cooldown");
         confirmButton.innerHTML = `<span>Submit</span>`; // Reset the button text
     });
