ef0f5576c828661247a49148906dbdff1d1881c4
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2024-02-16 23:35:06 +0800

    update

--- admin-side/ceap_information.php
+++ admin-side/ceap_information.php
@@ -713,6 +713,12 @@ for ($i = 0; $i < count($tempAccountRows); $i++) {
         echo '</tr>';
     }
 }
+ // Check if the current status is "Disqualified" and display the reason if so
+ if ($status == 'Disqualified' || $status ==  'Fail') {
+    echo '<tr>';
+    echo '<td colspan="3" style="font-style: italic;">Reason for Disqualification: <strong>' . $tempAccountRow['reason'] . '<strong></td>';
+    echo '</tr>';
+}
             ?>
         </tbody>
     </table>
