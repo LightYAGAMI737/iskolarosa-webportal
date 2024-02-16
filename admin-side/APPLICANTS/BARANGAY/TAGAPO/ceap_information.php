80fb34e438af79e476e20f3693918c7ed7a2f1cc
LightYAGAMI737 <132973896+LightYAGAMI737@users.noreply.github.com>
2024-02-16 22:47:23 +0800

    update

--- admin-side/APPLICANTS/BARANGAY/TAGAPO/ceap_information.php
+++ admin-side/APPLICANTS/BARANGAY/TAGAPO/ceap_information.php
@@ -405,15 +405,15 @@ for ($i = 0; $i < count($tempAccountRows); $i++) {
         $inProgressDisplayed = true; // Set the flag to true to indicate that the "In Progress" row has been displayed
     }
 
-    // Check if this row has a previous status
-    if (!empty($tempAccountRow['currentSTAT']) && $status != 'In Progress') {
-        // Display a new row for the previous status
-        echo '<tr>';
-        echo '<td data-label="Date:">' . $UpdatedDateFormatted . '</td>';
-        echo '<td data-label="Status:">' . strtoupper($tempAccountRow['currentSTAT']) . '</td>';
-        echo '<td data-label="Approved by:">' . ($status == 'In Progress' ? '-' : $updatedBy) . '</td>';
-        echo '</tr>';
-    }
+ // Check if this row has a previous status
+if (!empty($tempAccountRow['currentSTAT']) && $status != 'IN PROGRESS') {
+    // Display a new row for the previous status
+    echo '<tr>';
+    echo '<td data-label="Date:">' . $updatedDateFormatted . '</td>';
+    echo '<td data-label="Status:">' . ($status === 'Disqualified' ? $applicantReason : $tempAccountRow['currentSTAT']) . '</td>';
+    echo '<td data-label="Approved by:">' . $updatedBy . '</td>';
+    echo '</tr>';
+}
 }
             ?>
         </tbody>
